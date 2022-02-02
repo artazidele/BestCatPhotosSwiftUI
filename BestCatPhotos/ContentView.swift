c6640c58a741f9e028c8a43e11d31ca9d988f6ee
artazidele <77989249+artazidele@users.noreply.github.com>
2022-02-02 13:46:03 +0200

    Just call API to get list with 20 cat photos

--- BestCatPhotos/ContentView.swift
+++ BestCatPhotos/ContentView.swift
@@ -8,13 +8,73 @@
 import SwiftUI
 import FirebaseAuth
 
+struct URLImage: View {
+    let urlString: String
+    @State var data: Data?
+    var body: some View {
+        if let data = data, let uiimage = UIImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 80, height: 120)
+                .background(Color.brown)
+        } else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 80, height: 120)
+                .background(Color.brown)
+                .onAppear {
+                    fetch()
+                }
+        }
+    }
+    
+    private func fetch() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            self.data = data
+        }
+        task.resume()
+    }
+}
+struct CatPhoto: Hashable, Codable {
+    let id: String
+    let url: String
+}
+class PhotoViewModel: ObservableObject {
+    @Published var catPhotos: [CatPhoto] = []
+    func fetchData() {
+        guard let url = URL(string: "https://api.thecatapi.com/v1/images/search?limit=20") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let catPhotos = try JSONDecoder().decode([CatPhoto].self, from: data)
+                DispatchQueue.main.async {
+                    self?.catPhotos = catPhotos
+                }
+            } catch {
+                print(error)
+            }
+            
+        }
+        task.resume()
+    }
+}
 class AppViewModel: ObservableObject {
     let auth = Auth.auth()
     @Published var signedIn = false
     var isSignedIn: Bool {
         return auth.currentUser != nil
     }
-    
+
     func logIn(email: String, password: String) {
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] result, error in
             guard result != nil, error == nil else { return
@@ -24,7 +84,7 @@ class AppViewModel: ObservableObject {
             }
         }
     }
-    
+
     func signUp(email: String, password: String) {
         Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
             guard result != nil, error == nil else { return
@@ -34,7 +94,7 @@ class AppViewModel: ObservableObject {
             }
         }
     }
-    
+
     func signOut() {
         try? auth.signOut()
         self.signedIn = false
@@ -45,38 +105,38 @@ struct ContentView: View {
 //    @State var email = ""
 //    @State var password = ""
     
+    @StateObject var catPhotoViewModel = PhotoViewModel()
+    
     @EnvironmentObject var viewModel: AppViewModel
     
     var body: some View {
         NavigationView {
-            if viewModel.signedIn{
-                VStack {
-                    Text("You are signed in")
-                    Button(action: {
-                        viewModel.signOut()
-                    }, label: {
-                        Text("Sign Out")
-                    })
+            List {
+                ForEach(catPhotoViewModel.catPhotos, id: \.self) { catPhoto in
+                    HStack {
+                        URLImage(urlString: catPhoto.url)
+                        Text(catPhoto.id)
+                    }
                 }
-            } else {
-                SignInView()
             }
             
-//            VStack {
-//                TextField("Email: ", text: $email)
-//                SecureField("Password", text: $password)
-//                Button(action: {
-//                    guard !email.isEmpty, !password.isEmpty else {
-//                        return
-//                    }
-//                    viewModel.logIn(email: email, password: password)
-//                }, label: {
-//                    Text("Log In")
-//                })
+            
+//            if viewModel.signedIn{
+//                VStack {
+//                    Text("You are signed in")
+//                    Button(action: {
+//                        viewModel.signOut()
+//                    }, label: {
+//                        Text("Sign Out")
+//                    })
+//                }
+//            } else {
+//                SignInView()
 //            }
         }
         .onAppear {
             viewModel.signedIn = viewModel.isSignedIn
+            catPhotoViewModel.fetchData()
         }
     }
 }
