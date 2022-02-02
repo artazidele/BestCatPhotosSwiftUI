d247391de38b993ea1340abd93be67cc6dcc919f
artazidele <77989249+artazidele@users.noreply.github.com>
2022-02-02 16:25:34 +0200

    Continue making UI

--- BestCatPhotos/ContentView.swift
+++ BestCatPhotos/ContentView.swift
@@ -102,23 +102,104 @@ class AppViewModel: ObservableObject {
 }
 
 struct ContentView: View {
-//    @State var email = ""
-//    @State var password = ""
+    @State var email = ""
+    @State var password = ""
     
-    @StateObject var catPhotoViewModel = PhotoViewModel()
-    
-    @EnvironmentObject var viewModel: AppViewModel
+//    @StateObject var catPhotoViewModel = PhotoViewModel()
+//
+//    @EnvironmentObject var viewModel: AppViewModel
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(catPhotoViewModel.catPhotos, id: \.self) { catPhoto in
-                    HStack {
-                        URLImage(urlString: catPhoto.url)
-                        Text(catPhoto.id)
+        
+        VStack {
+            ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.red, lineWidth: 2)
+            ZStack {
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke(Color.red, lineWidth: 2)
+                VStack {
+                    VStack(alignment: .leading, spacing: 2) {
+                    Text("Email:")
+                            .padding()
+                        
+                    TextField("", text: $email)
+                        .autocapitalization(.none)
+                        .padding()
+            
+                    Text("Password:")
+                            .padding()
+                    
+                    SecureField("", text: $password)
+                        .autocapitalization(.none)
+                        .padding()
                     }
+                    Button(action: {
+                        guard !email.isEmpty, !password.isEmpty else {
+                            return
+                        }
+        //                viewModel.logIn(email: email, password: password)
+                    }, label: {
+                        Text("Log In")
+                            .padding()
+                            .background(Color.red)
+                            .tint(Color.white)
+                            .cornerRadius(4)
+                    })
                 }
             }
+            .padding(4)
+        }
+        .frame(width: 280, height: 320, alignment: .center)
+        .padding()
+            
+            Button(action: {
+                guard !email.isEmpty, !password.isEmpty else {
+                    return
+                }
+//                viewModel.logIn(email: email, password: password)
+            }, label: {
+                Text("HAVE NOT GOT ACCOUNT?")
+                    .tint(Color.red)
+            })
+                .padding()
+            
+            
+            Button(action: {
+                guard !email.isEmpty, !password.isEmpty else {
+                    return
+                }
+//                viewModel.logIn(email: email, password: password)
+            }, label: {
+                Text("FORGOT PASSWORD?")
+                    .tint(Color.red)
+            })
+                .padding()
+            
+//            NavigationLink("Have not got account?", destination: SignUpView())
+        }
+        
+        
+        
+//        NavigationView {
+//
+//            ZStack {
+//                RoundedRectangle(cornerRadius: 8)
+//                    .stroke(lineWidth: 2)
+//            }
+            
+            
+            
+            
+            
+//            List {
+//                ForEach(catPhotoViewModel.catPhotos, id: \.self) { catPhoto in
+//                    HStack {
+//                        URLImage(urlString: catPhoto.url)
+//                        Text(catPhoto.id)
+//                    }
+//                }
+//            }
             
             
 //            if viewModel.signedIn{
@@ -133,10 +214,10 @@ struct ContentView: View {
 //            } else {
 //                SignInView()
 //            }
-        }
+//        }
         .onAppear {
-            viewModel.signedIn = viewModel.isSignedIn
-            catPhotoViewModel.fetchData()
+//            viewModel.signedIn = viewModel.isSignedIn
+//            catPhotoViewModel.fetchData()
         }
     }
 }
